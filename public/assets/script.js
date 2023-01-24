71ffc295b140bed93e3f43484bc6553ca99f87e0
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-01-24 10:09:19 +0100

    add form validation with js into add post

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,159 +1,211 @@
-let iconLeft=document.querySelector(".icon-left")
-let iconRight=document.querySelector(".icon-right")
-let sideBar=document.querySelector(".side-bar")
-let words=document.querySelectorAll(".words")
-let mySection=document.querySelector(".my-section")
-let navBar=document.querySelector(".nav-bar")
-let BlogText=document.querySelector(".Blog-text")
-words.forEach(word=>{
-    word.style.display="none";
+let iconLeft = document.querySelector(".icon-left")
+let iconRight = document.querySelector(".icon-right")
+let sideBar = document.querySelector(".side-bar")
+let words = document.querySelectorAll(".words")
+let mySection = document.querySelector(".my-section")
+let navBar = document.querySelector(".nav-bar")
+let BlogText = document.querySelector(".Blog-text")
+words.forEach(word => {
+    word.style.display = "none";
 })
-if(iconLeft){
-    iconLeft.onclick=()=>{
+if (iconLeft) {
+    iconLeft.onclick = () => {
         iconLeft.classList.add("hidden");
         iconRight.classList.remove("hidden")
-        sideBar.style.width="70px";
-        words.forEach(word=>{
-            word.style.display="none";
+        sideBar.style.width = "70px";
+        words.forEach(word => {
+            word.style.display = "none";
         })
-        BlogText.style.display="none"
+        BlogText.style.display = "none"
     }
 }
-if(iconRight){
-    iconRight.onclick=()=>{
+if (iconRight) {
+    iconRight.onclick = () => {
         iconRight.classList.add("hidden")
         iconLeft.classList.remove("hidden")
-        sideBar.style.width="270px";
-        words.forEach(word=>{
-            word.style.display="block";
+        sideBar.style.width = "270px";
+        words.forEach(word => {
+            word.style.display = "block";
         })
-        BlogText.style.display="block"
+        BlogText.style.display = "block"
     }
 }
 
 // button addpost
 let addpost = document.querySelector("#add-post")
 let postModal = document.querySelector("#post-Modal")
-let closeAddPostModal=document.querySelector("#closeAddPost-modal")
+let closeAddPostModal = document.querySelector("#closeAddPost-modal")
 let declineModal = document.querySelector('#decline-modal')
-if(addpost){
-addpost.onclick=()=>{
-    postModal.classList.remove('hidden')
-}
-closeAddPostModal.onclick=()=>{
-    postModal.classList.add('hidden')
-}
-declineModal.onclick=()=>{
-    postModal.classList.add('hidden')
-}
+if (addpost) {
+    addpost.onclick = () => {
+        postModal.classList.remove('hidden')
+    }
+    closeAddPostModal.onclick = () => {
+        postModal.classList.add('hidden')
+    }
+    declineModal.onclick = () => {
+        postModal.classList.add('hidden')
+    }
 }
 // category
-let categoryModal=document.querySelector("#category-Modal")
-let addCategory=document.querySelector("#add-category")
-let declineCategoryModal=document.querySelector('#declineCategory-modal')
-let closeAddCategoryModal=document.querySelector('#closeAddCategory-modal')
-if(addCategory){
-addCategory.onclick=()=>{
-    document.querySelector("#update-category").style.display = 'none';
-    document.querySelector("#add").style.display = 'block';
-    categoryModal.classList.remove('hidden')
-    document.querySelector("#my-field").style.display="block"
-    document.querySelector('#drop-down').style.display="none"
-}
-closeAddCategoryModal.onclick=()=>{
-    categoryModal.classList.add('hidden')
-}
-declineCategoryModal.onclick=()=>{
-    categoryModal.classList.add('hidden')
-}
+let categoryModal = document.querySelector("#category-Modal")
+let addCategory = document.querySelector("#add-category")
+let declineCategoryModal = document.querySelector('#declineCategory-modal')
+let closeAddCategoryModal = document.querySelector('#closeAddCategory-modal')
+if (addCategory) {
+    addCategory.onclick = () => {
+        document.querySelector("#update-category").style.display = 'none';
+        document.querySelector("#add").style.display = 'block';
+        categoryModal.classList.remove('hidden')
+        document.querySelector("#my-field").style.display = "block"
+        document.querySelector('#drop-down').style.display = "none"
+    }
+    closeAddCategoryModal.onclick = () => {
+        categoryModal.classList.add('hidden')
+    }
+    declineCategoryModal.onclick = () => {
+        categoryModal.classList.add('hidden')
+    }
 }
 // alert success
-myAlert=document.querySelector("#my-alert")
-closeAlert=document.querySelector("#close-alert")
-if(closeAlert){
-    closeAlert.onclick=()=>{
+myAlert = document.querySelector("#my-alert")
+closeAlert = document.querySelector("#close-alert")
+if (closeAlert) {
+    closeAlert.onclick = () => {
         myAlert.classList.add('hidden');
     }
 }
 //  alert danger
-mydangerAlert=document.querySelector("#my-Dangeralert");
-closeDangerAlert=document.querySelector("#close-Dangeralert");
-if(closeDangerAlert){
-    closeDangerAlert.onclick=()=>{
+mydangerAlert = document.querySelector("#my-Dangeralert");
+closeDangerAlert = document.querySelector("#close-Dangeralert");
+if (closeDangerAlert) {
+    closeDangerAlert.onclick = () => {
         mydangerAlert.classList.add('hidden');
     }
 }
-if(document.querySelector("#update-category")){
+if (document.querySelector("#update-category")) {
     document.querySelector("#update-category").style.display = 'none';
 
 }
 
 
-if(document.querySelector('#drop-down')){
-    document.querySelector('#drop-down').style.display="none"}
-function editCategory(name,id){
+if (document.querySelector('#drop-down')) {
+    document.querySelector('#drop-down').style.display = "none"
+}
+function editCategory(name, id) {
     categoryModal.classList.remove('hidden');
     document.querySelector("#update-category").style.display = 'block';
     document.querySelector("#add").style.display = 'none';
     document.querySelector("#dorpDown-category").value = name;
-    document.querySelector("#category-id").value=id
-    document.querySelector("#my-field").style.display="none"
-    document.querySelector('#drop-down').style.display="block"
-    console.log(name,id)
+    document.querySelector("#category-id").value = id
+    document.querySelector("#my-field").style.display = "none"
+    document.querySelector('#drop-down').style.display = "block"
+    console.log(name, id)
 }
-function editPost(id,title,content,name,image){
+function editPost(id, title, content, name, image) {
     document.querySelector('#update-Modal').classList.remove("hidden");
-    document.querySelector('#title').value=title
-    document.querySelector('#posts-category').value=name;
-    document.querySelector("#image-up").setAttribute('src','public/images/'+image);
-    document.querySelector("#editor").value=content
-    document.querySelector("#post-id").value=id
+    document.querySelector('#title').value = title
+    document.querySelector('#posts-category').value = name;
+    document.querySelector("#image-up").setAttribute('src', 'public/images/' + image);
+    document.querySelector("#editor").value = content
+    document.querySelector("#post-id").value = id
 }
-if(document.querySelector('#closeUpdatePost-modal')){
-    document.querySelector('#closeUpdatePost-modal').onclick=()=>{
-    document.querySelector('#update-Modal').classList.add("hidden");
+if (document.querySelector('#closeUpdatePost-modal')) {
+    document.querySelector('#closeUpdatePost-modal').onclick = () => {
+        document.querySelector('#update-Modal').classList.add("hidden");
     }
 }
-if(document.querySelector('#declineUpdate-modal')){
-    document.querySelector('#declineUpdate-modal').onclick=()=>{
-    document.querySelector('#update-Modal').classList.add("hidden");
+if (document.querySelector('#declineUpdate-modal')) {
+    document.querySelector('#declineUpdate-modal').onclick = () => {
+        document.querySelector('#update-Modal').classList.add("hidden");
     }
 }
-let hideOverView=document.querySelector("#hide-overView")
-let myOverView=document.querySelector("#my-overView")
-let showOverView=document.querySelector("#show-overView")
-if(hideOverView){
-    hideOverView.onclick=()=>{
+let hideOverView = document.querySelector("#hide-overView")
+let myOverView = document.querySelector("#my-overView")
+let showOverView = document.querySelector("#show-overView")
+if (hideOverView) {
+    hideOverView.onclick = () => {
         myOverView.classList.add('hidden')
-        console.log('hide')
     }
 }
-function overview(title,content,name,image){
+function overview(title, content, name, image) {
     myOverView.classList.remove('hidden')
-    console.log(title)
-    console.log(content)
-    document.querySelector("#Opost-title").textContent=title
-    document.querySelector("#Opost-content").textContent=content
-    document.querySelector("#Opost-image").setAttribute('src','public/images/'+image)
-    document.querySelector("#Opsts-Category").textContent=name;
+    document.querySelector("#Opost-title").textContent = title
+    document.querySelector("#Opost-content").textContent = content
+    document.querySelector("#Opost-image").setAttribute('src', 'public/images/' + image)
+    document.querySelector("#Opsts-Category").textContent = name;
 }
-if(document.querySelector('#add-number')){
-  document.querySelector('#add-number').addEventListener('click',(e)=>{
-     if(document.querySelector("#number").value==""){
-        document.querySelector("#number").style.border="2px solid red"
-        document.querySelector('#add-error').classList.remove('hidden');
-        e.preventDefault();
-     }
-  })
-  document.querySelector('#number').onclick=()=>{
-        document.querySelector("#number").style.border=""
-        document.querySelector('#add-error').classList.add('hidden');
-
-  }
-  if(document.querySelector("#add-AllPosts")){
-    let postsTitle=document.querySelectorAll(".post-title ")
-    document.querySelector("#add-AllPosts").addEventListener('click',(e)=>{
-        
+if (document.querySelector('#add-number')) {
+    document.querySelector('#add-number').addEventListener('click', (e) => {
+        if (document.querySelector("#number").value == "") {
+            document.querySelector("#number").style.border = "2px solid red"
+            document.querySelector('#add-error').classList.remove('hidden');
+            e.preventDefault();
+        }
     })
-  }
+    document.querySelector('#number').onclick = () => {
+        document.querySelector("#number").style.border = ""
+        document.querySelector('#add-error').classList.add('hidden');
+    }
+}
+let addAllPosts = document.querySelector("#add-AllPosts")
+if (addAllPosts) {
+    let postsTitle = document.querySelectorAll(".post-title")
+    let postContent = document.querySelectorAll(".post-content")
+    regex = /^(?=.*[a-z])(?=.*[A-Z]).{50,}$/;
+    Titleregex = /^.{5,100}$/;
+
+    addAllPosts.addEventListener('click', (e) => {
+        postsTitle.forEach(element => {
+            if (element.value.trim() == '') {
+                element.value=""
+                element.style.border = "2px solid red"
+                element.setAttribute('placeholder','please fill this feild')
+                element.classList.add("placeholder-red")
+                e.preventDefault();
+            }
+            if(!Titleregex.test(element.value)){
+                element.value=""
+                element.style.border = "2px solid red"
+                element.setAttribute('placeholder','please enter a valid title')
+                element.classList.add("placeholder-red")
+                e.preventDefault();
+            }
+        });
+        postContent.forEach(element => {
+            if (element.value.trim() == '') {
+                element.value=""
+                element.style.border = "2px solid red"
+                element.setAttribute('placeholder','please fill this field')
+                element.classList.add("placeholder-red")
+                e.preventDefault();
+            }
+            if(!regex.test(element.value)){
+                element.value=""
+                element.style.border = "2px solid red"
+                element.setAttribute('placeholder','This text must contains at least 50 charcters')
+                element.classList.add("placeholder-red")
+                e.preventDefault();
+            }
+
+        });
+    })
+        postsTitle.forEach(element => {
+            element.onclick=()=>{
+                if (element.value.trim() == ''){
+                    element.value=""
+                    element.style.border = ""
+                    element.classList.remove("placeholder-red")
+                }
+            }
+        });
+        postContent.forEach(element => {
+            element.onclick=()=>{
+                if (element.value.trim() == ''){
+                    element.value=""
+                    element.style.border = ""
+                    element.classList.remove("placeholder-red")
+                }
+            }
+        });
 }
--- views/add.php
+++ views/add.php
@@ -4,70 +4,71 @@ $mydata = $category->GetUniqueCategory();
 if (isset($_POST["number_posts"])) {
     $_SESSION["post_number"] = $_POST["number_posts"];
 }
-if(isset($_POST["add-post"])){
+if (isset($_POST["add-post"])) {
     $post = new PostsController();
-    $post ->addPost();
+    $post->addPost();
 }
-require_once('includes/dashboard.php') 
+require_once('includes/dashboard.php')
 ?>
-    <header class="dashboard-header">
-        <nav class="nav-bar flex justify-between">
-            <div class="search-bar w-80">
-                <form>
-                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                    <div class="relative">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                            </svg>
-                        </div>
-                        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
-                        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+<header class="dashboard-header">
+    <nav class="nav-bar flex justify-between">
+        <div class="search-bar w-80">
+            <form>
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
                     </div>
-                </form>
+                    <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
+                    <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                </div>
+            </form>
 
-            </div>
-            <div class="menu-bar">
-                <i class="w-6 h-6 fa-solid fa-bars"></i>
-            </div>
-        </nav>
-    </header>
-    <section class="my-section flex justify-center mt-20">
-        <div class="w-4/5">
+        </div>
+        <div class="menu-bar">
+            <i class="w-6 h-6 fa-solid fa-bars"></i>
+        </div>
+    </nav>
+</header>
+<section class="my-section flex justify-center mt-20">
+    <div class="w-4/5">
         <form class="w-full" method="post" enctype="multipart/form-data">
             <?php for ($i = 0; $i < $_SESSION["post_number"]; $i++) { ?>
                 <div class="w-full px-4 border border-gray-200 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l rounded-lg mb-2">
-                        <div class="mb-6 pt-2">
-                            <label for="title" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title</label>
-                            <input  type="text" name="post-title[]" class="post-title title bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="title" required>
-                        </div>
-                        <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                            <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
-                                <label for="editor" class="sr-only">Publish post</label>
-                                <textarea  id="editor<?=$i?>" name="post-content[]" rows="8" class="post-content block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
-                            </div>
-                        </div>
-                        <div class="mb-6">
-                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                            <select name="post-category[]" id="dorpDown-category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <?php foreach ($mydata as $val) { ?>
-                                    <option value="<?= $val["id_category"] ?>"><?= $val["name"] ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="mb-6">
-                            <label for="picture" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Picture</label>
-                            <input type="file" name="post-picture[]" class="picture bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="image.png" required>
-                        </div>
+                    <div class="mb-6 pt-2">
+                        <label for="title" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title</label>
+                        <input type="text" name="post-title[]" class="post-title title bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="title">
+                        <p class="title-error hidden text-red-600 font-bold">you must fill this field</p>
+                    </div>
+                    <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="editor" class="sr-only">Publish post</label>
+                        <textarea id="editor<?= $i ?>" name="post-content[]" rows="8" class="post-content px-2 block w-full pt-1 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..."></textarea>
+                    </div>
+                    <p class="content-error hidden text-red-600 font-bold">you must fill this field</p>
+
+                    <div class="mb-6">
+                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                        <select name="post-category[]" id="dorpDown-category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <?php foreach ($mydata as $val) { ?>
+                                <option value="<?= $val["id_category"] ?>"><?= $val["name"] ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="mb-6">
+                        <label for="picture" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Picture</label>
+                        <input type="file" name="post-picture[]" class="picture bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="image.png">
+                    </div>
                 </div>
             <?php } ?>
             <div class="flex items-center justify-end">
                 <button id="add-AllPosts" name="add-post" type="submit" class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Add <?php echo $_SESSION["post_number"] > 1 ? 'Posts' :  'Post' ?></button>
                 <a href="posts" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">back</a>
             </div>
-            </form>
+        </form>
 
-        </div>
+    </div>
 
-    </section>
+</section>
 </main>
